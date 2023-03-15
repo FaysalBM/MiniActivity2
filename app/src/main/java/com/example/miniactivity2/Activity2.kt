aa55d1ca86e6df0f4c3bf7866ae893a6d1e302c2
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-03-15 18:40:20 +0100

    improved graphics

--- app/src/main/java/com/example/miniactivity2/Activity2.kt
+++ app/src/main/java/com/example/miniactivity2/Activity2.kt
@@ -1,11 +1,25 @@
 package com.example.miniactivity2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.miniactivity2.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = Activity2Binding.inflate(layoutInflater)
+
+        binding.messageResult.text = intent.getStringExtra("messageInput")?.repeat(intent.getIntExtra("repsInput", 0))
+
+        binding.backButton.setOnClickListener {
+            setResult(
+                RESULT_OK,
+                Intent().putExtra("messageResult", binding.messageResult.text.toString())
+            )
+            finish()
+        }
+
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/miniactivity2/MainActivity.kt
+++ app/src/main/java/com/example/miniactivity2/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.miniactivity2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.miniactivity2.databinding.ActivityMainBinding
 
+lateinit var getContent : ActivityResultLauncher<Intent>
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonBye.setOnClickListener {
+            val messageInput = binding.message5.text.toString()
+            val repsInput = binding.message3.text.toString().toIntOrNull()
+
+            val intent = Intent(this, Activity2::class.java).apply {
+                putExtra("messageInput", messageInput)
+                putExtra("repsInput", repsInput)
+            }
+            getContent.launch(intent)
+        }
+
+        getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { input ->
+            if (input?.resultCode == RESULT_OK) {
+                val messageInput = input.data?.getStringExtra("messageInput")
+                binding.message6.text = messageInput
+                binding.message5.text.clear()
+                binding.message3.text.clear()
+                binding.message3.requestFocus()
+            }
+        }
     }
+
+
 }
